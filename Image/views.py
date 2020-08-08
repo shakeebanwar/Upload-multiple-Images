9b723ff06f811722404534cfa4a29e6abd7d3457
shakeeb <shakeebanwar250@gmail.com>
2020-08-08 11:54:24 +0500

    some changes

--- Image/views.py
+++ Image/views.py
@@ -10,19 +10,26 @@ def upload(request):
     if request.method == "POST":
         files = request.FILES.getlist('images')
         product = request.POST['product']
-        photoData = Photo(title = product)
-        photoData.save()
+        checkExist = Photo.objects.filter(title = product)
+        if checkExist:
+            return HttpResponse('Product Already Exist in DataBase Make sure title must be Unique')
         
-        for i in range(len(files)):
-            imagesData = MultipleImages(photoDetails = Photo.objects.get(title = product),img = files[i])
-            imagesData.save()
+        else:
+            photoData = Photo(title = product)
+            photoData.save()
+            
+            for i in range(len(files)):
+                imagesData = MultipleImages(photoDetails = Photo.objects.get(title = product),img = files[i])
+                imagesData.save()
 
-        
-        photodata = Photo.objects.get(title = product)
-        data = MultipleImages.objects.filter(photoDetails = photodata.id)
+            
+            photodata = Photo.objects.get(title = product)
+            data = MultipleImages.objects.filter(photoDetails = photodata.id)
 
     
         return render(request,'slider.html',{'data':data})
+
+    return render(request,'check.html')
         
 
 
